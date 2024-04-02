2d3ac91cce6517e6a36d7ad99ebab36bc12c28be
Alan Oliveira <alanprot7@gmail.com>
2024-04-02 00:33:47 -0300

    Adicionando funcao de calcular media

--- scripts_v1.js
+++ scripts_v1.js
@@ -1,6 +1,6 @@
-const  pontuacao = document.getElementById('pontuacao')
-const  escore = document.getElementById('escore')
-const  resultado = document.getElementById('resultado')
+// Globals
+
+let countNotas = 1
 
 document.addEventListener('click', (e) => {
     if(e.target.id === 'calcular'){
@@ -10,6 +10,15 @@ document.addEventListener('click', (e) => {
     }
     if(e.target.id === 'limpar'){
         limpar()
+    }
+    if(e.target.id === 'plus'){
+        adicionarNota()
+    }
+    if(e.target.id === 'zerar'){
+        zerar()
+    }
+    if(e.target.id === 'calcular-media'){
+        calcularMedia()
     }
 })
 
@@ -27,7 +36,53 @@ const limpar = () => {
     escore.value = ''
 }
 
+const zerar = () => {
+    countNotas = 1
+    resultadoMedia.innerHTML = ''
+    notas.innerHTML = ''
+    notas.innerHTML += `<label for="nota">Digite a Nota ${countNotas}:</label>`
+    notas.innerHTML += `<input type="number" id="nota1" name="nota"/>`
+}
+
 const montar = (valor) => {
     let html = `<h3>Nota Obtida:</h3><h1>${valor}</h1>`
     resultado.innerHTML = html
 }
+
+const adicionarNota = () => {
+    countNotas++
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `nota${countNotas}`);
+    label.textContent = `Digite a Nota ${countNotas}:`;
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'number');
+    input.setAttribute('id', `nota${countNotas}`);
+    input.setAttribute('name', 'nota');
+
+    notas.appendChild(label);
+    notas.appendChild(input);
+
+}
+
+const calcularMedia = () => {
+    let nodes = notas.childNodes
+    let countNodes = 0
+    let sumNodeValues = 0.0
+    nodes.forEach(element => {
+        if (element.nodeName.toLowerCase() == 'input' && element.value !== ''){
+            sumNodeValues += parseFloat(element.value.replace(',','.'))
+            countNodes++
+        }
+    });
+    if (countNodes > 0)
+        if (countNodes > 1){
+            let result = sumNodeValues / countNodes
+            resultadoMedia.innerHTML = `<h3>Média Obtida:</h3><h1>${result.toFixed(1).replace('.',',')}</h1>`     
+        }else {
+            resultadoMedia.innerHTML = `<h3>Média Obtida:</h3><h1>${sumNodeValues.toFixed(1).replace('.',',')}</h1>`
+        }
+}
+
+
